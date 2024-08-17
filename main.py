d46ca31fab73cf92f1769f19d6c67753d81fa8b7
AlexMixa2012 <157954023+AlexMixa2012@users.noreply.github.com>
2024-08-17 17:20:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
+from geopy.geocoders import Nominatim
 
+
+geolocator = Nominatim(user_agent="fire_locator")
+fire_coords = (34.0522, -118.2437)
+location = geolocator.reverse(fire_coords, language='ru')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def firehere(ctx):
+    await ctx.send(location.address)
+
+bot.run("TOKEN")
