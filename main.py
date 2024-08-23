d227877594937349b8128e897c76ce48b07c00f8
AlexMixa2012 <157954023+AlexMixa2012@users.noreply.github.com>
2024-08-23 20:02:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import discord
 from discord.ext import commands
-from geopy.geocoders import Nominatim
-
-
-geolocator = Nominatim(user_agent="fire_locator")
-fire_coords = (34.0522, -118.2437)
-location = geolocator.reverse(fire_coords, language='ru')
+import OWM
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+tokyo = ""
+
+@bot.command()
+async def city(ctx, *, data):
+    global tokyo
+    tokyo = data
+    await ctx.send(f"Your city is {data}")
+
 @bot.command()
 async def firehere(ctx):
-    await ctx.send(location.address)
+    OWM.setup(tokyo)
+    await ctx.send(OWM.textfire)
 
-bot.run("TOKEN")
+bot.run("token")
