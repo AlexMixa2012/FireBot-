38ec25e8c5527574804e9009e01160215ea9ebc7
AlexMixa2012 <157954023+AlexMixa2012@users.noreply.github.com>
2024-08-31 12:17:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,36 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-tokyo = ""
+kuba = ""
+
+@bot.command()
+async def lang(ctx, *, langa):
+    OWM.lang = langa
+    if OWM.lang == "en":
+        await ctx.send(f"Your language :{langa}")
+    if OWM.lang == "es":
+        await ctx.send(f"Tu idioma :{langa}")
+    if OWM.lang == "ru":
+        await ctx.send(f"Ваш язык :{langa}")
+    if OWM.lang == "lv":
+        await ctx.send(f"Tava valoda :{langa}")
 
 @bot.command()
 async def city(ctx, *, data):
-    global tokyo
-    tokyo = data
-    await ctx.send(f"Your city is {data}")
+    global kuba
+    kuba = data
+    if OWM.lang == "en":
+        await ctx.send(f"Your city is {data}")
+    if OWM.lang == "es":
+        await ctx.send(f"Tu ciudad es {data}")
+    if OWM.lang == "ru":
+        await ctx.send(f"Ваш город {data}")
+    if OWM.lang == "lv":
+        await ctx.send(f"Tava pilsēta {data}")
 
 @bot.command()
 async def firehere(ctx):
-    OWM.setup(tokyo)
+    OWM.setup(kuba)
     await ctx.send(OWM.textfire)
 
-bot.run("token")
+bot.run("Token")
